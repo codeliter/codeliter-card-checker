5ee7eb55090dc20bbdd3b71b5f8b6257cb2385f9
Abolarin Stephen <hackzlord@gmail.com>
2016-10-29 11:20:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="A simple payment interface for making transactions in a more securaed way">
+    <meta name="description" content="A simple interface for checking card validity">
     <meta name="author" content="CodeLiter">
 
     <title>CodeLiter Card Checker</title>
