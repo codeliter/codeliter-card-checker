2f5853de9122f14ee21d9a3561d33e5dcb73659e
Abolarin Stephen <hackzlord@gmail.com>
2016-10-29 11:07:44 +0100

    Update checker.php

--- checker.php
+++ checker.php
@@ -4,21 +4,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-<<<<<<< HEAD
-
-require_once('class/BinChecker.php');
-
-if ($_POST) {
-
-	$card_no	= (int) $_POST['card_no'];	// Credit Card number
-
-=======
 require_once('class/BinChecker.php');
 
 if ($_POST) {
 	// Credit Card number
 	$card_no	= (int) $_POST['card_no'];	
->>>>>>> e1434bda879ac0544d6a7671bd134c565b3711e0
 	//Empty error to store errors
 	$error= array();
 
@@ -34,10 +24,6 @@ if ($_POST) {
 		}
 	}
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e1434bda879ac0544d6a7671bd134c565b3711e0
 	// If any error was thrown
 	if (count($error) > 0) {
 		// Save error to session
@@ -56,21 +42,10 @@ if ($_POST) {
 			$_SESSION['msg']= ['error'=>$checker->message];
 			header('Location: index.php');
 		}
-<<<<<<< HEAD
-		
 	}
-
-
-=======
-	}
->>>>>>> e1434bda879ac0544d6a7671bd134c565b3711e0
 }
 else {
 	$_SESSION['msg']= ['error'=>"Please try again!"];
 	header('Location: index.php');
 }
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> e1434bda879ac0544d6a7671bd134c565b3711e0
